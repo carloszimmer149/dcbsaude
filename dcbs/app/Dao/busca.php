e2527c35e3b89a678b183c6c41baa3232f1f8ba5
Carlos Zimmer <carlos.zimmer@protonmail.com>
2022-04-09 22:37:15 -0300

    projetova_conexao_help

--- dcbs/app/Dao/busca.php
+++ dcbs/app/Dao/busca.php
@@ -12,9 +12,9 @@ class Busca{
         $sql = "SELECT count (casosNovos, Recuperadonovos, obitosNovos) FROM covid22 ";
         $con = $mysqli->query($sql) or die($mysqli->error);
         while($dado = $con->fetch_array()){
-            $dbconfirmados = $dado["casosNovos"];
-            $dbrecuperados = $dado["Recuperadonovos"];
-            $obitos = $dado["obitosNovos"];
+            $dbconfirmados["casosNovos"];
+            $dbrecuperados["Recuperadonovos"];
+            $dbobitos["obitosNovos"];
         }
         mysqli_close($mysqli);
         
